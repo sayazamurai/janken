aaaa37053d2095dc789039cfd9b4e37e223b7853
Saya <sayazamurai38@gmail.com>
2019-02-27 22:58:37 -0800

    add janken

--- src/App.js
+++ src/App.js
@@ -2,38 +2,134 @@ import React, { Component } from 'react'
 import 'tachyons'
 import './App.css'
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+// 0 = ぐー
+// 1 = ちょき
+// 2 = ぱー
+
+const getRival = () => {
+  const result = getRandomInt(3)
+  if (result === 0) {
+    return 'ぐー'
+  }
+  if (result === 1) {
+    return 'ちょき'
+  }
+  if (result === 2) {
+    return 'ぱー'
+  }
+}
+
 class App extends Component {
   state = {
-    number: 0
+    win: 0,
+    lose: 0,
+    me: '?',
+    rival: '?'
   }
 
-  buttonClickPlus = () => {
+  buttonClickGoo = () => {
     this.setState({
-      number: this.state.number + 1
+      me: 'ぐー'
     })
+    const rival = getRival()
+    this.setState({ rival: rival })
+
+    if (rival === 'ちょき') {
+      this.setState({
+        win: this.state.win + 1
+      })
+    }
+    if (rival === 'ぱー') {
+      this.setState({
+        lose: this.state.lose + 1
+      })
+    }
   }
 
-  buttonClickMinus = () => {
+  buttonClickChoki = () => {
     this.setState({
-      number: this.state.number - 1
+      me: 'ちょき'
+    })
+    const rival = getRival()
+    this.setState({ rival: rival })
+
+    if (rival === 'ぱー') {
+      this.setState({
+        win: this.state.win + 1
+      })
+    }
+    if (rival === 'ぐー') {
+      this.setState({
+        lose: this.state.lose + 1
+      })
+    }
+  }
+
+  buttonClickPa = () => {
+    this.setState({
+      me: 'ぱー'
+    })
+    const rival = getRival()
+    this.setState({ rival: rival })
+
+    if (rival === 'ぐー') {
+      this.setState({
+        win: this.state.win + 1
+      })
+    }
+    if (rival === 'ちょき') {
+      this.setState({
+        lose: this.state.lose + 1
+      })
+    }
+  }
+
+  buttonClickReset = () => {
+    this.setState({
+      win: 0,
+      lose: 0,
+      me: '?',
+      rival: '?'
     })
   }
 
   render() {
     return (
       <div className="avenir tc pt5">
-        <div className="mb4 f2">{this.state.number}</div>
+        <div className="mb2 f3">相手 {this.state.rival}</div>
+        <div className="mb4 f3">自分 {this.state.me}</div>
+        <div>
+          <button
+            onClick={this.buttonClickGoo}
+            className="link dim br1 ba ph3 pv2 mb2 dib dark-pink b--dark-pink"
+          >
+            ぐー
+          </button>{' '}
+          <button
+            onClick={this.buttonClickChoki}
+            className="link dim br1 ba ph3 pv2 mb2 dib dark-pink b--dark-pink"
+          >
+            ちょき
+          </button>{' '}
+          <button
+            onClick={this.buttonClickPa}
+            className="link dim br1 ba ph3 pv2 mb2 dib dark-pink b--dark-pink"
+          >
+            ぱー
+          </button>
+        </div>
+        <div className="mt4 mb4 f3">
+          {this.state.win}勝{this.state.lose}敗
+        </div>
         <button
-          onClick={this.buttonClickPlus}
-          className="f3 link dim br1 ba ph3 pv1 mb2 dib dark-pink b--dark-pink"
+          onClick={this.buttonClickReset}
+          className="link dim br1 ba ph3 pv2 mb2 dib dark-pink b--dark-pink"
         >
-          +
-        </button>{' '}
-        <button
-          onClick={this.buttonClickMinus}
-          className="f3 link dim br1 ba ph3 pv1 mb2 dib dark-pink b--dark-pink"
-        >
-          —
+          やり直す
         </button>
       </div>
     )
