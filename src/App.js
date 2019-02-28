5c1aa93c55fb12fc0f49d8a4c7d912ea7694fb04
Saya <sayazamurai38@gmail.com>
2019-02-27 22:05:24 -0800

    add +&-

--- src/App.js
+++ src/App.js
@@ -3,9 +3,40 @@ import 'tachyons'
 import './App.css'
 
 class App extends Component {
+  state = {
+    number: 0
+  }
+
+  buttonClickPlus = () => {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+
+  buttonClickMinus = () => {
+    this.setState({
+      number: this.state.number - 1
+    })
+  }
+
   render() {
-    const number = 5
-    return <div className="avenir dark-gray lh-copy tc pt5">{number}</div>
+    return (
+      <div className="avenir tc pt5">
+        <div className="mb4 f2">{this.state.number}</div>
+        <button
+          onClick={this.buttonClickPlus}
+          className="f3 link dim br1 ba ph3 pv1 mb2 dib dark-pink b--dark-pink"
+        >
+          +
+        </button>{' '}
+        <button
+          onClick={this.buttonClickMinus}
+          className="f3 link dim br1 ba ph3 pv1 mb2 dib dark-pink b--dark-pink"
+        >
+          —
+        </button>
+      </div>
+    )
   }
 }
 
